30db1e2e9838e2c6eed99c2516aec40a41a05257
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-02 20:19:49 +0900

    Initial Commit

---
+++ janken/jankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenApp.swift
+//  janken
+//
+//  Created by 木村朋広 on 2024/02/02.
+//
+
+import SwiftUI
+
+@main
+struct jankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
